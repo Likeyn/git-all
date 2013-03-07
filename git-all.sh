25e3933eef22f5b7c6fde74d970354390c12054b
Pierre-François CLEMENT <likeyn@gmail.com>
2013-03-07 10:46:52 +0000

    Made git-all compatible with git-achievements

--- git-all.sh
+++ git-all.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Bash script that uses find to trigger a git action
 # on all subdirectories that contain a git repository
 # except in folders that matches $IGN pattern
@@ -6,10 +6,17 @@
 # Init
 DIR='';
 CMD='';
+GIT='git';
 IGN='*_ARCHIVES_*';
 
+# Check if 'git-achievements' exists, and use it
+# @link https://github.com/icefox/git-achievements
+if command -v 'git-achievements' &> /dev/null; then
+	GIT='git-achievements';
+fi;
+
+# Loop through first bash args to detect dirs
 if [ $# -gt 0 ]; then
-  # Loop through first bash args to detect dirs
 	for arg; do
 		if [ -d $arg ]; then DIR="$DIR $arg"; shift;
 		else break; fi;
@@ -20,5 +27,5 @@ else
 	exit 1;
 fi;
 
-find $DIR -path "$IGN" -prune -o -type d -name .git -printf "\n---[\033[1;34m%h\033[0m]---\n" -execdir git --no-pager $CMD \;
+find $DIR -path "$IGN" -prune -o -type d -name .git -printf "\n---[\033[1;34m%h\033[0m]---\n" -execdir $GIT --no-pager $CMD \;
 echo -e "\n";
